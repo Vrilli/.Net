1a502e9d8f301eff8ef0051b5b66686c4505c567
vrilli <vrillyjcr@gmail.com>
2024-06-22 22:34:10 -0500

    primeros pasos en .net

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Por favor ingrese su nombre");
+var nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo");
+var cargo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su edad");
+var edad = Console.ReadLine();
+Console.WriteLine($"Mi nombre es {nombre}, mi cargo es {cargo}, y tengo {edad} años");
